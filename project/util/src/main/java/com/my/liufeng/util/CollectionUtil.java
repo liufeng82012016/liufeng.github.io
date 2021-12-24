46b1ecaeabd2877512d17ac5989c0ed1b090d1a9
liuyanghong <liuyanghong1@duiba.com.cn>
2021-12-24 16:15:15 +0800

    新增util包

---
+++ project/util/src/main/java/com/my/liufeng/util/CollectionUtil.java
@@ -0,0 +1,26 @@
+package com.my.liufeng.util;
+
+import java.util.Collection;
+
+public class CollectionUtil {
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String s) {
+        return s == null || s.trim().length() == 0;
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static <T> boolean isEmpty(T[] args) {
+        return args == null || args.length == 0;
+    }
+
+    /**
+     * 判断集合是否为空
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+}
